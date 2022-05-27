96a44d153eee31f95cd6b3e300ce018138c6aef9
aayush4vedi <4vedi.aayush@gmail.com>
2022-05-27 20:38:40 +0530

    hopefully should work-2

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,18 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { MoralisProvider } from "react-moralis";
 import { theme } from "../styles";
 
+
+
 function MyApp({ Component, pageProps }) {
+
+  //hacky env key handling
+  NEXT_PUBLIC_APP_ID = "17lcszN16P19yIIkMvRhTkNgROEtuFujHdFlEgtt"
+  NEXT_PUBLIC_SERVER_URL = "https://lflj3dtwafxr.usemoralis.com:2053/server"
   return (
     <ChakraProvider theme={theme}>
       <MoralisProvider
-        appId={process.env.NEXT_PUBLIC_APP_ID}
-        serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
+        appId={NEXT_PUBLIC_APP_ID}
+        serverUrl={NEXT_PUBLIC_SERVER_URL}
       >
         <Component {...pageProps} />
       </MoralisProvider>
