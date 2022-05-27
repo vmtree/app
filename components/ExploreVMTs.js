7861f111daa5dd96100c4036f9a6f0ca32aba539
aayush4vedi <4vedi.aayush@gmail.com>
2022-05-27 20:31:02 +0530

    hopefully should work-2

--- components/ExploreVMTs.js
+++ components/ExploreVMTs.js
@@ -19,7 +19,140 @@ import { arboristAddress } from "../constants/addresses";
 import TableHeader from "./TableHeader";
 import TreeEntry from "./TreeEntry";
 
+// const ExploreVMTs = (props) => {
+
+//   // treats arborist contract as backend database
+//   const getVMTreesOptions = {
+//     chain: "rinkeby",
+//     address: arboristAddress,
+//     functionName: "getVMTrees",
+//     abi: arboristAbi,
+//     params: {},
+//   };
+//   // useApiContract connects to moralis provider without needing user's wallet
+//   // to be connected
+//   const {
+//     runContractFunction,
+//     data,
+//     error,
+//     isLoading,
+//     isFetching,
+//   } = useApiContract(getVMTreesOptions);
+//   // get the tree data when the page loads
+//   useEffect(() => {
+//     runContractFunction();
+
+//     console.log("[ExploreVMTs] ........................>>");
+//     console.log(
+//       "........................ appId: ",
+//       process.env.NEXT_PUBLIC_APP_ID
+//     );
+//     console.log(
+//       "........................ serverUrl: ",
+//       process.env.NEXT_PUBLIC_SERVER_URL
+//     );
+//     console.log("........................ data: ", data);
+//     console.log(
+//       "........................ getVMTreesOptions: ",
+//       getVMTreesOptions
+//     );
+
+//   },[]);    // eslint-disable-line react-hooks/exhaustive-deps
+
+//   if(!data){
+//     return (
+//       <Box paddingLeft="7%" paddingRight="7%">
+//       <Flex>
+//         <Text fontSize="xl" ml="2em" fontWeight="bold">
+//           Explore VMTs
+//         </Text>
+//       </Flex>
+
+//       <Box>
+//         <Flex
+//           pl="5em"
+//           pr="5em"
+//           fontSize="xx-small"
+//           minWidth="max-content"
+//           alignItems="center"
+//           gap="2"
+//         >
+//           <Flex>
+//             <Box w="100%" h="10" bg="blue.500" />
+//             <GridItem w="100%" h="10" bg="blue.500" />
+//           </Flex>
+//           <Flex>
+//             <GridItem w="100%" h="10" bg="blue.500" />
+//             <GridItem w="100%" h="10" bg="blue.500" />
+//             <GridItem w="100%" h="10" bg="blue.500" />
+//           </Flex>
+//         </Flex>
+//         <TableHeader/>
+//         <Box pt={10}>
+//         <Stack ml="2em" mr="2em">
+//           <Skeleton height='50px' borderRadius={155}/>
+//           <Skeleton height='50px' borderRadius={155}/>
+//           <Skeleton height='50px' borderRadius={155}/>
+//           <Skeleton height='50px' borderRadius={155}/>
+//           <Skeleton height='50px' borderRadius={155}/>
+//           <Skeleton height='50px' borderRadius={155}/>
+//         </Stack>
+//         </Box>
+//       </Box>
+//     </Box>
+//     )
+//   }
+
+//   return (
+//     <Box paddingLeft="7%" paddingRight="7%">
+//       <Flex>
+//         <Text fontSize="xl" ml="2em" fontWeight="bold">
+//           Explore VMTs
+//         </Text>
+//       </Flex>
+
+//       <Box>
+//         <Flex
+//           pl="5em"
+//           pr="5em"
+//           fontSize="xx-small"
+//           minWidth="max-content"
+//           alignItems="center"
+//           gap="2"
+//         >
+//           <Flex>
+//             <Box w="100%" h="10" bg="blue.500" />
+//             <GridItem w="100%" h="10" bg="blue.500" />
+//           </Flex>
+//           <Flex>
+//             <GridItem w="100%" h="10" bg="blue.500" />
+//             <GridItem w="100%" h="10" bg="blue.500" />
+//             <GridItem w="100%" h="10" bg="blue.500" />
+//           </Flex>
+//         </Flex>
+//         {/* <Button onClick={runContractFunction()} /> */}
+//         <TableHeader />
+//         <Box>
+//           {data &&
+//             data[1].map((vmtree, index) => {
+//               // The struct returns data in an array!
+//               //   struct VMTreeData {
+//               //     string name;
+//               //     bool isActive;
+//               //     address contractAddress;
+//               //     address controller;
+//               //     address linkPayer;
+//               //   }
+//               return <TreeEntry myTrees={data[1]} key={data[1]} />;
+//             })}
+//         </Box>
+//       </Box>
+//     </Box>
+//   );
+// };
+
 const ExploreVMTs = (props) => {
+
   // treats arborist contract as backend database
   const getVMTreesOptions = {
     chain: "rinkeby",
@@ -30,71 +163,17 @@ const ExploreVMTs = (props) => {
   };
   // useApiContract connects to moralis provider without needing user's wallet
   // to be connected
-  const { runContractFunction, data, error, isLoading, isFetching } =
-    useApiContract(getVMTreesOptions);
+  const {
+    runContractFunction,
+    data,
+    error,
+    isLoading,
+    isFetching,
+  } = useApiContract(getVMTreesOptions);
   // get the tree data when the page loads
   useEffect(() => {
-    runContractFunction();
-
-    console.log("[ExploreVMTs] ........................>>");
-    console.log(
-      "........................ appId: ",
-      process.env.NEXT_PUBLIC_APP_ID
-    );
-    console.log(
-      "........................ serverUrl: ",
-      process.env.NEXT_PUBLIC_SERVER_URL
-    );
-    console.log("........................ data: ", data);
-    console.log(
-      "........................ getVMTreesOptions: ",
-      getVMTreesOptions
-    );
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-  if (!data) {
-    return (
-      <Box paddingLeft="7%" paddingRight="7%">
-        <Flex>
-          <Text fontSize="xl" ml="2em" fontWeight="bold">
-            Explore VMTs
-          </Text>
-        </Flex>
-
-        <Box>
-          <Flex
-            pl="5em"
-            pr="5em"
-            fontSize="xx-small"
-            minWidth="max-content"
-            alignItems="center"
-            gap="2"
-          >
-            <Flex>
-              <Box w="100%" h="10" bg="blue.500" />
-              <GridItem w="100%" h="10" bg="blue.500" />
-            </Flex>
-            <Flex>
-              <GridItem w="100%" h="10" bg="blue.500" />
-              <GridItem w="100%" h="10" bg="blue.500" />
-              <GridItem w="100%" h="10" bg="blue.500" />
-            </Flex>
-          </Flex>
-          <TableHeader />
-          <Box pt={10}>
-            <Stack ml="2em" mr="2em">
-              <Skeleton height="50px" borderRadius={155} />
-              <Skeleton height="50px" borderRadius={155} />
-              <Skeleton height="50px" borderRadius={155} />
-              <Skeleton height="50px" borderRadius={155} />
-              <Skeleton height="50px" borderRadius={155} />
-              <Skeleton height="50px" borderRadius={155} />
-            </Stack>
-          </Box>
-        </Box>
-      </Box>
-    );
-  }
+    runContractFunction()
+  }, []);     // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <Box paddingLeft="7%" paddingRight="7%">
@@ -105,6 +184,7 @@ const ExploreVMTs = (props) => {
       </Flex>
 
       <Box>
+        {/* Header row */}
         <Flex
           pl="5em"
           pr="5em"
@@ -123,11 +203,41 @@ const ExploreVMTs = (props) => {
             <GridItem w="100%" h="10" bg="blue.500" />
           </Flex>
         </Flex>
-        {/* <Button onClick={runContractFunction()} /> */}
-        <TableHeader />
+        <SimpleGrid
+          columns={25}
+          spacing={0.5}
+          fontSize="xx-small"
+          color="gray.400"
+        >
+          <Text bg="#fff"></Text>
+          <Text bg="#fff">STATUS</Text>
+          <Text bg="#fff"></Text>
+          <Text bg="#fff"></Text>
+          <Text bg="#fff"></Text>
+          <Text bg="#fff">ADDRESS</Text>
+          <Text bg="#fff"></Text>
+          <Text bg="#fff"></Text>
+          <Text bg="#fff"></Text>
+          <Text bg="#fff"></Text>
+          <Text bg="#fff"></Text>
+          <Text bg="#fff"></Text>
+          <Text bg="#fff">NAME</Text>
+          <Text bg="#fff"></Text>
+          <Text bg="#fff"></Text>
+          <Text bg="#fff">BALANCE</Text>
+          <Text bg="#fff"></Text>
+          <Text bg="#fff"></Text>
+          <Text bg="#fff">USAGE</Text>
+          <Text bg="#fff"></Text>
+          <Text bg="#fff">CREATED</Text>
+          <Text bg="#fff"></Text>
+          <Text bg="#fff"></Text>
+          <Text bg="#fff"></Text>
+          <Text bg="#fff"></Text>
+        </SimpleGrid>
+
         <Box>
-          {data &&
-            data[1].map((vmtree, index) => {
+        { data && data[1].map((vmtree, index) => {
               // The struct returns data in an array!
               //   struct VMTreeData {
               //     string name;
@@ -136,8 +246,39 @@ const ExploreVMTs = (props) => {
               //     address controller;
               //     address linkPayer;
               //   }
-              return <TreeEntry myTrees={data[1]} key={data[1][2]} />;
-            })}
+              return (
+                <Flex
+                justifyContent="space-between"
+                backgroundColor="#fff"
+                borderRadius={155}
+                boxShadow="md"
+                padding="1em"
+                pl="5em"
+                pr="5em"
+                fontSize="smaller"
+                color="gray.600"
+                margin="1em"
+                key={vmtree[2]}
+              >
+                <Text>
+                    { vmtree[1] ? <Image src="enabled_vmt.svg" alt="enabled" />
+                        : <Image src="disabled_vmt.svg" alt="disabled" />
+                    }
+                </Text>
+                <Text>{/* name */ vmtree[0]}</Text>
+                <Text>{/* contractAddress */ vmtree[2]}</Text>
+                <Text>{/* controller */ vmtree[3]}</Text>
+                <Text>{/* linkPayer */ vmtree[4]}</Text>
+                <Text isNumeric>{/* linkPayerBalance */ }</Text>
+                <Text>
+                  <Link href="#">
+                    <SettingsIcon />
+                  </Link>
+                </Text>
+              </Flex>
+              );
+            })
+          }
         </Box>
       </Box>
     </Box>
--- components/ManageVMTs.js
+++ components/ManageVMTs.js
@@ -27,6 +27,161 @@ function isNumberString(arg) {
     return /^[0-9]{1,78}$/g.test(arg);
 }
 
+// const ManageVMTs = (props) => {
+//   const [myTrees, setMyTrees] = useState([]);
+//   const [linkPayer, setLinkPayer] = useState();
+//   const [amount, setAmount] = useState();
+//   // form handlers
+//   const handleLinkPayer = (e) => setLinkPayer(e.target.value);
+//   const handleAmount = (e) => setAmount(e.target.value);
+//   // errors
+//   const linkPayerError = !linkPayer || !isAddress(linkPayer);
+//   const amountError = !amount || !isNumberString(amount);
+
+//   // treats arborist contract as backend database
+//   const getVMTreesOptions = {
+//     chain: "rinkeby",
+//     address: arboristAddress,
+//     functionName: "getVMTrees",
+//     abi: arboristAbi,
+//     params: {},
+//   };
+//   // useApiContract connects to moralis provider without needing user's wallet
+//   // to be connected
+//   const { runContractFunction, data, error, isLoading, isFetching } =
+//     useApiContract(getVMTreesOptions);
+//   // get the tree data when the page loads
+//   useEffect(() => {
+//     runContractFunction();
+//   }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+//   // to make transactions, we need to be connected to the user's wallet.
+//   // the useMoralis hook gets the account from the ConnectButton and gives us
+//   // a class to make transactions with.
+//   const { Moralis, isWeb3Enabled, account } = useMoralis();
+
+//   // filter trees when the user connects. it shows all trees for which an
+//   // account is either the controller or the linkPayer
+//   useEffect(() => {
+//     if (data && account) {
+//       const trees = data[1].filter((vmtree) => {
+//         return (
+//           getAddress(vmtree[3]) == getAddress(account) ||
+//           getAddress(vmtree[2]) == getAddress(account)
+//         );
+//       });
+//       setMyTrees(trees);
+
+//       console.log("[ManageVMTs] ........................>>");
+//       console.log(
+//         "........................ appId: ",
+//         process.env.NEXT_PUBLIC_APP_ID
+//       );
+//       console.log(
+//         "........................ serverUrl: ",
+//         process.env.NEXT_PUBLIC_SERVER_URL
+//       );
+//       console.log("........................ myTrees: ", myTrees);
+//       console.log("........................ linkPayer: ", linkPayer);
+//       console.log(
+//         "........................ getVMTreesOptions: ",
+//         getVMTreesOptions
+//       );
+//     }
+//   }, [data, account]); // eslint-disable-line react-hooks/exhaustive-deps
+
+//   // for each contract interaction that we can do with the VMTree, we want
+//   // a pair of functionNameOptions and functionName objects to do the tx.
+//   // then we add a form + button that calls the functionName
+//   const topUpLinkOptions = () => ({
+//     chain: "rinkeby",
+//     address: arboristAddress,
+//     functionName: "topUpLink",
+//     abi: arboristAbi,
+//     params: { linkPayer, amount },
+//   });
+//   async function topUpLink() {
+//     try {
+//       const tx = await Moralis.executeFunction(topUpLinkOptions());
+//       const receipt = await tx.wait();
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
+
+//   if (!isWeb3Enabled || !account || !myTrees.length) return null;
+
+//   return (
+//     <Box paddingLeft="7%" paddingRight="7%">
+//       <Flex>
+//         <Text fontSize="xl" ml="2em" fontWeight="bold">
+//           Manage VMTs
+//         </Text>
+//       </Flex>
+
+//       <Box>
+//         {/* Header row */}
+//         <Flex
+//           //   justifyContent="center"
+//           pl="5em"
+//           pr="5em"
+//           fontSize="xx-small"
+//           minWidth="max-content"
+//           alignItems="center"
+//           gap="2"
+//         >
+//           <Flex>
+//             <Box w="100%" h="10" bg="blue.500" />
+//             <GridItem w="100%" h="10" bg="blue.500" />
+//           </Flex>
+//           <Flex>
+//             <GridItem w="100%" h="10" bg="blue.500" />
+//             <GridItem w="100%" h="10" bg="blue.500" />
+//             <GridItem w="100%" h="10" bg="blue.500" />
+//           </Flex>
+//         </Flex>
+
+//         <Box>
+//           <SimpleGrid
+//             columns={25}
+//             spacing={0.5}
+//             fontSize="xx-small"
+//             color="gray.400"
+//           >
+//             <Text bg="#fff"></Text>
+//             <Text bg="#fff">STATUS</Text>
+//             <Text bg="#fff"></Text>
+//             <Text bg="#fff"></Text>
+//             <Text bg="#fff"></Text>
+//             <Text bg="#fff">ADDRESS</Text>
+//             <Text bg="#fff"></Text>
+//             <Text bg="#fff"></Text>
+//             <Text bg="#fff"></Text>
+//             <Text bg="#fff"></Text>
+//             <Text bg="#fff"></Text>
+//             <Text bg="#fff"></Text>
+//             <Text bg="#fff">NAME</Text>
+//             <Text bg="#fff"></Text>
+//             <Text bg="#fff"></Text>
+//             <Text bg="#fff">BALANCE</Text>
+//             <Text bg="#fff"></Text>
+//             <Text bg="#fff"></Text>
+//             <Text bg="#fff">USAGE</Text>
+//             <Text bg="#fff"></Text>
+//             <Text bg="#fff">CREATED</Text>
+//             <Text bg="#fff"></Text>
+//             <Text bg="#fff"></Text>
+//             <Text bg="#fff"></Text>
+//             <Text bg="#fff"></Text>
+//           </SimpleGrid>
+
+//           <TreeEntry myTrees={myTrees} key={myTrees[2]} />
+//         </Box>
+//       </Box>
+//     </Box>
+//   );
+// };
+
 const ManageVMTs = (props) => {
   const [myTrees, setMyTrees] = useState([]);
   const [linkPayer, setLinkPayer] = useState();
@@ -53,7 +208,7 @@ const ManageVMTs = (props) => {
   // get the tree data when the page loads
   useEffect(() => {
     runContractFunction();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []);       // eslint-disable-line react-hooks/exhaustive-deps
 
   // to make transactions, we need to be connected to the user's wallet.
   // the useMoralis hook gets the account from the ConnectButton and gives us
@@ -71,24 +226,8 @@ const ManageVMTs = (props) => {
         );
       });
       setMyTrees(trees);
-
-      console.log("[ManageVMTs] ........................>>");
-      console.log(
-        "........................ appId: ",
-        process.env.NEXT_PUBLIC_APP_ID
-      );
-      console.log(
-        "........................ serverUrl: ",
-        process.env.NEXT_PUBLIC_SERVER_URL
-      );
-      console.log("........................ myTrees: ", myTrees);
-      console.log("........................ linkPayer: ", linkPayer);
-      console.log(
-        "........................ getVMTreesOptions: ",
-        getVMTreesOptions
-      );
     }
-  }, [data, account]); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [data, account]);
 
   // for each contract interaction that we can do with the VMTree, we want
   // a pair of functionNameOptions and functionName objects to do the tx.
@@ -175,7 +314,49 @@ const ManageVMTs = (props) => {
             <Text bg="#fff"></Text>
           </SimpleGrid>
 
-          <TreeEntry myTrees={myTrees} />
+          {myTrees.map((vmtree, index) => {
+            // The struct returns data in an array!
+            //   struct VMTreeData {
+            //     string name;
+            //     bool isActive;
+            //     address contractAddress;
+            //     address controller;
+            //     address linkPayer;
+            //   }
+            return (
+              <Flex
+                justifyContent="space-between"
+                backgroundColor="#fff"
+                borderRadius={155}
+                boxShadow="md"
+                padding="1em"
+                pl="5em"
+                pr="5em"
+                fontSize="smaller"
+                color="gray.600"
+                margin="1em"
+                key={vmtree[2]}
+              >
+                <Text>
+                  {vmtree[1] ? (
+                    <Image src="enabled_vmt.svg" alt="enabled" />
+                  ) : (
+                    <Image src="disabled_vmt.svg" alt="disabled" />
+                  )}
+                </Text>
+                <Text>{/* name */ vmtree[0]}</Text>
+                <Text>{/* contractAddress */ vmtree[2]}</Text>
+                <Text>{/* controller */ vmtree[3]}</Text>
+                <Text>{/* linkPayer */ vmtree[4]}</Text>
+                <Text isNumeric>{/* linkPayerBalance */}</Text>
+                <Text>
+                  <Link href="#">
+                    <SettingsIcon />
+                  </Link>
+                </Text>
+              </Flex>
+            );
+          })}
         </Box>
       </Box>
     </Box>
