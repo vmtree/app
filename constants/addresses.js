ec25b5a50842de2bc1a9c7667e8ab0bfd9d11eba
aayush4vedi <4vedi.aayush@gmail.com>
2022-05-27 21:00:25 +0530

    trying with netlify

--- constants/addresses.js
+++ constants/addresses.js
@@ -2,4 +2,7 @@
 module.exports = {
     arboristAddress: '0xff41a716d5d8555491b3e58ae051765369f19148',
     linkTokenAddress: '0x01BE23585060835E02B77ef475b0Cc51aA1e0709',
+    //hacky env key handling
+    NEXT_PUBLIC_APP_ID: '17lcszN16P19yIIkMvRhTkNgROEtuFujHdFlEgtt',
+    NEXT_PUBLIC_SERVER_URL: 'https://lflj3dtwafxr.usemoralis.com:2053/server'
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { MoralisProvider } from "react-moralis";
 import { theme } from "../styles";
-
+import { NEXT_PUBLIC_APP_ID, NEXT_PUBLIC_SERVER_URL } from "../constants/addresses";
 
 
 function MyApp({ Component, pageProps }) {
 
-  //hacky env key handling
-  NEXT_PUBLIC_APP_ID = "17lcszN16P19yIIkMvRhTkNgROEtuFujHdFlEgtt"
-  NEXT_PUBLIC_SERVER_URL = "https://lflj3dtwafxr.usemoralis.com:2053/server"
   return (
     <ChakraProvider theme={theme}>
       <MoralisProvider
